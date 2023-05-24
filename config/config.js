bfaa360f119b9239bbede9884f5b50937ba4793e
Anderson Garnica <steven14.as@gmail.com>
2023-05-24 01:51:23 -0500

    cambio url

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'dev',
-  isProd: process.env.NODE_ENV ==='postgresql://postgres:6D2CFZmrFQlbYWcUEY0U@containers-us-west-163',
+  isProd: process.env.NODE_ENV ==='postgresql://postgres:6D2CFZmrFQlbYWcUEY0U@containers-us-west-163.railway.app:6463/railway',
   port: process.env.PORT || 3000,
   dbUrl: process.env.DATABASE_URL,
   apiKey: process.env.API_KEY,
